95b4376c28e6a7574375f77bc2c159efbf1ffbe0
Joel Rhoden <joelrhodenjm@gmail.com>
2022-11-15 08:12:20 -0500

    Added Javascript

---
+++ Application.js
@@ -0,0 +1,59 @@
+class UserLogin{
+    username = "";
+    password = "";
+
+    createUser(username, password){
+        username = username;
+        password = password;
+    }
+
+    isUser(){
+
+    }
+
+    validateUser(){
+
+    }
+
+    setPassword(){
+
+    }
+
+}
+
+
+class Customer{
+    name = "";
+    phoneNumber = 0;
+    emailAddress = "";
+    deviceID = "";
+    balance = 0;
+
+    Customer(name, phoneNumber, emailAddress, deviceID, balance){
+        name = name;
+        phoneNumber = phoneNumber;
+        emailAddress = emailAddress;
+        deviceID = deviceID;
+        balance = balance;
+    }
+
+    linkDevice(){
+
+    }
+
+    sendUpdates(){
+
+    }
+
+    addPayment(){
+
+    }
+
+    updateBalance(){
+
+    }
+
+    addCustomer(){
+        
+    }
+}
