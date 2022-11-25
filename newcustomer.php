dc4f75f83c7cf36a363aeab37d436852cfd25b17
Natonya Stewart <91899843+natonyastewart@users.noreply.github.com>
2022-11-25 14:18:20 -0500

    Update newcustomer.php

--- newcustomer.php
+++ newcustomer.php
@@ -7,15 +7,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Comp2140 Project</title>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	  	<link rel="stylesheet" href="dashboard.css">
-		<script src="app.js?v=1"></script>
+	    <link rel="stylesheet" href="dashboard.css">
 		</head>
 	<body>
 		<?php include 'header.php';?>
 		<div class="container">
 			<div class="back">
 				<div class="buttons">
-					<div><a href="dashboard.php"><i class="fa fa-home" aria-hidden="true"></i>Home</a></div>
+				<div><a href="dashboard.php"><i class="fa fa-home" aria-hidden="true"></i>Home</a></div>
 					<div><a href="newcustomer.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i>New Customer</a></div>
 					<div><a href="processorder.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Process Orders</a></div>
 					<div><a href="users.php"><i class="fa fa-users" aria-hidden="true"></i>Users</a></div>
@@ -28,13 +27,52 @@
 			</div>	
 			<div class="background">
 				<div class="records">
-					<div class="top-button">
-						<h1>New Customer</h1>
-					</div>	
+					<h1>New Customer</h1>
 					<div class="record2">
-						<div class="tables">
+						<form method="post" id="form" onsubmit="popup()" action="addcontact.php"> 
+							<div class="cell">	
+								<label for="title">Title</label><br>
+								<select id="title" name="title">
+									<option value="Mr.">Mr</option>
+									<option value="Mrs.">Mrs</option>
+									<option value="Ms.">Ms</option>
+								</select>
 							</div>
-						</div>
+							<div class="table">
+								<div class="cell">
+									<label for="fname">First Name</label>
+									<input type="text" name="fname" id="fname" required/>
+								</div>
+								<div class="cell">
+									<label for="lname">Last Name</label>
+									<input type="text" name="lname" id="lname" required/>
+								</div>
+							</div>
+							<div class="table">
+								<div class="cell">
+									<label for="email">Email</label>
+									<input type="email" name="email" id="email" required/>
+								</div>
+								<div class="cell">
+									<label>Telephone</label>
+									<input type="text" name="telephone" id="telephone" required/>
+								</div>
+							</div>
+							<div class="cell">
+								<label for="assigned-to">Assigned To</label><br>
+								<select id="assigned-to" name="assigned-to">
+									<option value="2">Joel Rhoden</option>
+									<option value="3">Natonya Stewart</option>
+									<option value="4">Ricardo Munda</option>
+									<option value="5">Rayon Hart</option>
+									<option value="6">Aalyah Johnson</option>
+									<option value="6">Jusayne Chambers</option>
+								</select>
+							</div><br>
+							<div class="save-button">
+								<button type="submit" id="save">Save</button>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
